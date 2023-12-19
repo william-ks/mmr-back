3759b97fd7dd3ef9e1b36d023f878de3529e5a40
William Krisley <will.ks2101@gmail.com>
2023-12-19 19:07:42 -0300

    Folder tree

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaGlobalModule } from './config/prisma/prisma.module';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [PrismaGlobalModule],
 })
 export class AppModule {}
